61bb767b42001a19b95603433f73642ab04566a6
Jack Greenwald <greenwald.j@northeastern.edu>
2026-04-30 16:34:09 -0400

    Made changing direction more responsive and added snakesize

--- main.cpp
+++ main.cpp
@@ -75,11 +75,9 @@ void loadScreen(std::vector<int> spaces, int currentSpace, int size)
     refresh();
 };
 
-std::vector<int> generateSpaces(int size)
+std::vector<int> generateSpaces(int size, int randomNum)
 {
     std::vector<int> spaces(size * size);
-    srand(time(0));
-    int randomNum = (rand() % (size * size - 1)) + 1; // Picks a random square that doesn't include the first square
     for (int x = 0; x < size; ++x)
     {
         for (int y = 0; y < size; ++y)
@@ -100,17 +98,17 @@ std::vector<int> generateSpaces(int size)
 int main()
 {
     int size = 9;
-
-    int currentSpace = 10;
+    int currentSpace = 0;
     std::vector<int> spaces;
-    spaces = generateSpaces(size);
-    std::string str = std::accumulate(spaces.begin(), spaces.end(), std::string(""),
-                                      [](const std::string &a, int b)
-                                      { return a + std::to_string(b); });
+
+    srand(time(0));
+    int randomNum = (rand() % (size * size - 1)) + 1; // Picks a random square that doesn't include the first square
+
+    spaces = generateSpaces(size, randomNum);
+
     initscr();
     cbreak();
     noecho();
-    // printw(str.c_str());
 
     loadScreen(spaces, currentSpace, size);
 
@@ -118,9 +116,11 @@ int main()
 
     char key = getch();
     clear();
-    // char key = 'w';
 
     char direction = 'r';
+
+    int loopNum = 0;
+    int snakeSize = 1;
     do
     {
         if (key == 'w' || key == 'A')
@@ -140,32 +140,41 @@ int main()
             direction = 'l';
         }
 
-        if (direction == 'l')
-        {
-            if (currentSpace % size != 0)
+        if (loopNum == 4) {
+            if (direction == 'l')
             {
-                currentSpace--;
+                if (currentSpace % size != 0)
+                {
+                    currentSpace--;
+                }
             }
-        }
-        else if (direction == 'r')
-        {
-            if (currentSpace % size != size - 1)
+            else if (direction == 'r')
             {
-                currentSpace++;
+                if (currentSpace % size != size - 1)
+                {
+                    currentSpace++;
+                }
             }
-        } else if (direction == 'u') {
-            if (currentSpace > size - 1) {
-                currentSpace -= size;
+            else if (direction == 'u')
+            {
+                if (currentSpace > size - 1)
+                {
+                    currentSpace -= size;
+                }
             }
-        } else if (direction == 'd') {
-            if (currentSpace < size * (size - 1)) {
-                currentSpace += size;
+            else if (direction == 'd')
+            {
+                if (currentSpace < size * (size - 1))
+                {
+                    currentSpace += size;
+                }
             }
+            loopNum = 0;
         }
         
-
         loadScreen(spaces, currentSpace, size);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        loopNum++;
 
         key = getch();
         clear();
